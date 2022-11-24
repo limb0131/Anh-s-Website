9c805397d37e88fcf07d38e3a4173ba8d176e26a
limb0131 <brianlim0201@gmail.com>
2022-11-24 05:57:44 -0600

    working icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            A.B.S <i className="fa-solid fa-magnifying-glass" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fa-solid fa-xmark" : "fas fa-bars"} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
